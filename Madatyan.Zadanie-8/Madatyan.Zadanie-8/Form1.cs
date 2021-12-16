1470c8a70b92410b2aac93823d3216b8a7b10f53
Denis <94359582+Madatyan@users.noreply.github.com>
2021-12-16 22:41:56 +0300

    Madatyan.Zadanie-8

---
+++ Madatyan.Zadanie-8/Madatyan.Zadanie-8/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a;
+            a = Convert.ToInt32(textBox1.Text);
+            if (comboBox1.SelectedIndex == 0)
+            {
+                a = (a * 135);
+                textBox2.Text = Convert.ToString(a);
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                a = (a * 215);
+                textBox2.Text = Convert.ToString(a);
+            }
+            if (comboBox1.SelectedIndex == 2)
+            {
+                a = (a * 414);
+                textBox2.Text = Convert.ToString(a);
+            }
+        }
+    }
+}
